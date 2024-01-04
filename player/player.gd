8e60c8e8d06eb4815dff14af25883aeeb8459326
G0ldenSp00n <hayfithyan+github.com/G0ldenSp00n@gmail.com>
2024-01-03 20:54:24 -0800

    mining: added mining speed and break delay

--- player/player.gd
+++ player/player.gd
@@ -15,9 +15,29 @@ var FLY_VELOCITY = -150.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var tileHealth = []
+
+func _ready():
+	var dimensions = tileMap.get_used_rect().size
+	for y in dimensions.y:
+		tileHealth.append([])
+		for x in dimensions.x:
+			var tileMapData = tileMap.get_cell_tile_data(MINING_LAYER, Vector2i(x, y))
+			if tileMapData:
+				tileHealth[y].append(tileMapData.get_custom_data_by_layer_id(0))
+
 func refuel(amount):
 	if FUEL < MAX_FUEL:
 		FUEL += amount;
+
+var MINING_SPEED = 80;
+func mine_cell(action, offsetPos, delta):
+	var tileMapPos = tileMap.local_to_map(global_position) + offsetPos
+	
+	tileHealth[tileMapPos.y][tileMapPos.x] -= MINING_SPEED * delta
+	print(tileHealth[tileMapPos.y][tileMapPos.x])
+	if tileHealth[tileMapPos.y][tileMapPos.x] <= 0:
+		tileMap.erase_cell(MINING_LAYER, tileMapPos)
 
 const FUEL_LABEL_TEXT = " Fuel - %d"
 func _physics_process(delta):
@@ -35,17 +55,18 @@ func _physics_process(delta):
 	
 	if is_on_floor():
 		if Input.is_action_pressed("move_down"):
-			tileMap.erase_cell(MINING_LAYER, tileMap.local_to_map(global_position) + Vector2i(0, 1))
+			mine_cell("move_down", Vector2i(0, 1), delta)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
-	
-	if is_on_wall():
+
+	if is_on_wall() and is_on_floor():
 		if Input.is_action_pressed("move_left"):
-			tileMap.erase_cell(MINING_LAYER, tileMap.local_to_map(global_position) + Vector2i(-1, 0))
+			mine_cell("move_left", Vector2i(-1, 0), delta)
 		if Input.is_action_pressed("move_right"):
-			tileMap.erase_cell(MINING_LAYER, tileMap.local_to_map(global_position) + Vector2i(1, 0))
+			mine_cell("move_right", Vector2i(1, 0), delta)
+
 	if direction and is_on_floor():
 		velocity.x = direction * SPEED
 	elif direction:
--- rock_droid_merchant/rock_merchant.gd
+++ rock_droid_merchant/rock_merchant.gd
@@ -2,6 +2,8 @@ extends Sprite2D
 
 @export var REFUEL_RATE = 20;
 
+signal refuel(amount);
+
 func _ready():
 	var animation_player = (%AnimationPlayer as AnimationPlayer);
 	animation_player.current_animation = "idle";
@@ -11,4 +13,4 @@ func _process(delta):
 	var players = (%RefuelArea as Area2D).get_overlapping_bodies();
 	if players.size() > 0:
 		for player in players:
-			player.refuel(REFUEL_RATE * delta);
+			refuel.emit(REFUEL_RATE * delta);
